f2f82690354eb182f03c6251ae1527f871d64623
grebwerd <grebwerd@gmail.com>
2018-08-29 20:52:41 -0600

    binary tree traversal preorder, inorder, and postorder and a node class

---
+++ practice/binaryTreeTraversal.py
@@ -0,0 +1,35 @@
+from node import Node
+def printPreorder(root):
+    if root:
+        print(root.val)
+        printPreorder(root.left)
+        printPreorder(root.right)
+
+
+def printInorder(root):
+    if root:
+        printInorder(root.left)
+        print(root.val)
+        printInorder(root.right)
+
+def printPostorder(root):
+     if root:
+        printPostorder(root.left)
+        printPostorder(root.right)
+        print(root.val)
+
+if __name__ == '__main__':
+   root = Node(1)
+   root.left = Node(2)
+   root.right = Node(3)
+   root.left.left = Node(4)
+   root.left.right = Node(5)
+   
+   print("The preorder traversal")
+   printPreorder(root)
+
+   print("The inorder traversal")
+   printInorder(root)
+
+   print("The postorder traversal")
+   printPostorder(root)
