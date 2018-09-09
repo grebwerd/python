28502ba30fdb2a4b32efbeebfaf2da32e5733893
grebwerd <grebwerd@gmail.com>
2018-09-08 23:29:11 -0600

    3Sum edit

--- practice/threeSum.py
+++ practice/threeSum.py
@@ -4,15 +4,48 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        nums.sort()
+        sumMap = self.sumPair(nums)
+        sets = set()
+        
+        retval = []
+        
+        if len(nums) < 3 or nums is None:
+            return []
+        
+        
+        for index, value in enumerate(nums):
+            if value in sumMap:
+                lists = sumMap.get(value)
+                for l in lists:
+                    index1 = l[0]
+                    index2 = l[1]
+                    if (index is not index1) and (index is not index2):
+
+                        if value < nums[index1]:
+                            sets.add(tuple([value, nums[index1], nums[index2]]))
+                        elif value < nums[index2]:
+                             sets.add(tuple([nums[index1], value, nums[index2]]))
+                        else:
+                            sets.add(tuple([nums[index1], nums[index2], value]))
+
+
+        
+        retval.extend(list(sets))
+        return retval
+                
 
 
 
     def sumPair(self, nums):
-        sums = set()
-        print("The length of nums is ",len(nums))
+        sumMap = {}
         for index, value in enumerate(nums):
-            print("The index is ", index)
             if index+1 < len(nums):
                 for nextIndex in range (index+1, len(nums)):
-                    print("the nextIndex is", nextIndex)
-                    sums.add(nums[index]+nextIndex)
+                    summ = (nums[index]+nums[nextIndex]) * -1
+                    if summ in sumMap.keys():
+                        sumMap[summ].append([index, nextIndex])        
+                    else:
+                        sumMap[summ] = [[index, nextIndex]]
+                    
+        return sumMap
